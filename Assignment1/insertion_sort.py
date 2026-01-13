53a5f3f5c4541ce217ac3d312f0d3fb15d0cb408
Aashish <ashishsapkota14@gmail.com>
2026-01-12 18:05:10 -0700

    first commit

---
+++ Assignment1/insertion_sort.py
@@ -0,0 +1,25 @@
+"""
+Insertion Sort Algorithm - Monotonically Decreasing Order
+MSCS 532 - Assignment 1
+
+This program implements the Insertion Sort algorithm to sort an array
+in decreasing order (from largest to smallest).
+"""
+
+def insertion_sort_decreasing(arr):
+    """
+    Sort an array in decreasing order using Insertion Sort.
+    
+    Args:
+        arr: List of numbers to be sorted
+        
+    Returns:
+        The sorted array in decreasing order
+    """
+    # TODO: Implement insertion sort
+    pass
+
+# Test the function
+if __name__ == "__main__":
+    test_array = [5, 2, 4, 6, 1, 3]
+    print("Original array:", test_array)
